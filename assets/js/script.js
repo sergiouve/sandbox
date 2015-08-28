5e3dae075191aec4ca61b0f52c7c469b09e5b298
Sergio Uve <sergiouve@gmail.com>
2015-08-28 16:46:17 +0200

    Playing around...

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,16 @@ var $slider = $('.slider.left');
 $(window).scroll(function() {
     if($(this).scrollTop() > 10) {
         $slider.stop().animate({left: '0px'}, 200);
-    } else {
-        $slider.stop().animate({left: '-1000px'}, 300);
     }
 });
 
 $(function() {
     $(".slider").each(function() {
-        var hue = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
-        $(this).css("background-color", hue);
+        var color = 'rgb(' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ',' + (Math.floor((256-199)*Math.random()) + 200) + ')';
+        $(this).css("background-color", color);
     });
+});
+
+$('.slider').mouseover(function() {
+    $(this).css("opacity", '1');
 });
